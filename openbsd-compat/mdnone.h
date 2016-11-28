4a3a19b16d3a9a01cec84fe397af3e97ff26aaa0
Greg <greg.mclearn@lightshipsec.com>
2016-11-27 20:59:59 -0500

    Fixed up preamble

--- openbsd-compat/mdnone.h
+++ openbsd-compat/mdnone.h
@@ -1,15 +1,6 @@
-/*	$OpenBSD: md5.h,v 1.17 2012/12/05 23:19:57 deraadt Exp $	*/
-
 /*
- * This code implements the MD5 message-digest algorithm.
- * The algorithm is due to Ron Rivest.  This code was
- * written by Colin Plumb in 1993, no copyright is claimed.
- * This code is in the public domain; do with it what you wish.
- *
- * Equivalent code is available from RSA Data Security, Inc.
- * This code has been tested against that, and is equivalent,
- * except that you don't need to include two pages of legalese
- * with every copy.
+ * This code implements the 'none' message-digest resulting
+ * in no integrity protection.
  */
 
 #ifndef _MDNONE_H_
